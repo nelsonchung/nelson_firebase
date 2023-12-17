9d333d7b461d472a88daca1f10c445e279979e25
Nelson Chung <chihchun.chung@gmail.com>
2023-12-17 13:20:25 +0800

    1. 可以新增資料到cloud firestore的資料庫 2. 可以顯示cloud firestore的現有資料有哪些

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,177 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Vending Machine Inventory',
+      home: VendingMachineList(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class VendingMachineList extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _VendingMachineListState createState() => _VendingMachineListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _VendingMachineListState extends State<VendingMachineList> {
+  String? selectedId, selectedLocation, selectedModel;
+  final List<String> availableIds = ["TW-000001", "TW-000002", "TW-000003", "TW-000004", "TW-000005", "TW-000006", "TW-000007", "TW-000008", "TW-000009", "TW-000010",
+                                "TW-000011", "TW-000012", "TW-000013", "TW-000014", "TW-000015", "TW-000016", "TW-000017", "TW-000018", "TW-000019", "TW-000020"]; // 替換成您的 ID 列表
+  final List<String> locationList = ["新光三越百貨-台北信義", "新光三越百貨-台中中港", "新光三越百貨-高雄", "太平洋崇光百貨-台北", "太平洋崇光百貨-台中", "太平洋崇光百貨-高雄",
+                                      "遠東百貨-板橋", "遠東百貨-竹北", "Big City遠東巨城購物中心"
+                                      ]; // 替換成您的地點列表
+  final List<String> modelList = ["type-36", "type-40", "type-42"];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  //List<String> availableIds; // 可用 ID 列表
+
+  //_VendingMachineListState() : availableIds = [...]; // 初始化可用 ID 列表
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Vending Machines'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('vending_machine').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          }
+          final data = snapshot.data;
+          if (data == null) {
+            return Text('No data found');
+          }
+          return ListView(
+            children: data.docs.map((DocumentSnapshot document) {
+              Map<String, dynamic> vendingMachine = document.data() as Map<String, dynamic>;
+              return ListTile(
+                title: Text(vendingMachine['model']),
+                subtitle: Text('ID: ${vendingMachine['id']} - Location: ${vendingMachine['location']}'),
+              );
+            }).toList(),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () => _addVendingMachineDialog(context),
+        tooltip: 'Add Vending Machine',
+        child: Icon(Icons.add),
+      ),
     );
   }
+
+// 這個函數可以放在 _VendingMachineListState 類別中
+Future<void> _addVendingMachineDialog(BuildContext context) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button for close dialog
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Add new vending machine'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              DropdownButton<String>(
+                value: selectedId,
+                hint: Text("Select machine ID"),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedId = newValue!;
+                  });
+                },
+                items: availableIds.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              DropdownButton<String>(
+                value: selectedLocation,
+                hint: Text("Select machine location"),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedLocation = newValue!;
+                  });
+                },
+                items: locationList.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              DropdownButton<String>(
+                value: selectedModel,
+                hint: Text("Select machine model"),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedModel = newValue!;
+                  });
+                },
+                items: modelList.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: Text('Cancel'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          TextButton(
+            child: Text('Add'),
+            onPressed: () {
+              if (selectedId != null && selectedLocation != null && selectedModel != null) {
+                FirebaseFirestore.instance
+                    .collection('vending_machine')
+                    .doc(selectedId) // 使用 selectedId 作為文檔 ID
+                    .set({
+                      'id': selectedId,
+                      'location': selectedLocation,
+                      'model': selectedModel,
+                    })
+                    .then((value) {
+                      setState(() {
+                        // 新增成功後，從可用 ID 列表中移除
+                        availableIds.remove(selectedId);
+                        selectedId = null; // 重置選擇的 ID
+                        selectedLocation = null; // 重置選擇的地點
+                        selectedModel = null; // 重置選擇的型號
+                      });
+                      print("Vending Machine Added");
+                    })
+                    .catchError((error) => print("Failed to add vending machine: $error"));
+                Navigator.of(context).pop();
+              }
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+
 }
